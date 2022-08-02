ccabc8cf9b2d2d6d2e7797568e3dcd417555baaf
Lucas Zanazzo <lucaszanazzo4@gmail.com>
2022-08-02 10:28:43 -0300

    Escudo
    -Capacidad de absorver daño
    - Animacion
    - Input del escudo

---
+++ Juego/Naves/Player/Escudo/Escudo.gd
@@ -0,0 +1,56 @@
+class_name Escudo 
+extends Area2D
+
+##Variables export
+export var energia: float= 8.0
+export var radio_desgaste: float = -1.6
+## Variables 
+var esta_activado: bool = false setget ,get_esta_activado
+
+##Setters y Getters
+func get_esta_activado():
+	return esta_activado
+
+#Metodos
+func _ready():
+	set_process(false)
+	controlar_colisionador(true)
+
+func _process(delta):
+	energia += radio_desgaste * delta
+	if energia <= 0.0:
+		desactivar()
+
+#Metodos custom 
+func controlar_colisionador(esta_desactivado: bool):
+	$CollisionShape2D.set_deferred("disabled", esta_desactivado)
+
+func activar():
+	if energia <= 0.0:
+		return
+	
+	print("activar")
+	esta_activado = true
+	controlar_colisionador(false)
+	$AnimationPlayer.play("default")
+
+func desactivar():
+	set_process(false)
+	esta_activado = false
+	controlar_colisionador(true)
+	$AnimationPlayer.play_backwards("default")
+
+##Señales internas 
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "default" and esta_activado:
+		$AnimationPlayer.play("activado")
+		set_process(true)
+
+
+func _on_Escudo_body_entered(body):
+	body.queue_free()
+
+
+
+func _on_Escudo_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	area.queue_free()
--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -23,6 +23,7 @@ onready var estela: Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx: Motor = $MotorSFX
 onready var colisionador: CollisionShape2D = $CollisionShape2D
 onready var impacto_sfx: AudioStreamPlayer = $ImpactoSFX
+onready var escudo: Escudo = $Escudo
 
 
 ## Metodos 
@@ -38,23 +39,27 @@ func _unhandled_input(event: InputEvent):
 	#Esta vivo el jugador? 
 	if not esta_input_activo():
 		return 
+
 	#Disparo_rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)
-	
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
-	# Control Estela 
+		
+	# Control Estela y sonido motor
 	if event.is_action_pressed("mover_adelante"):
 		estela.set_max_points(estela_maxima)
 		motor_sfx.sonido_on()
-
 	elif event.is_action_pressed("mover_atras"):
 		estela.set_max_points(0)
 		motor_sfx.sonido_on()
-	
 	if (event.is_action_released("mover_adelante") or event.is_action_released("mover_atras")):
 		motor_sfx.sonido_off()
+	
+	#Escudo 
+	if event.is_action_pressed("escudo") and not escudo.get_esta_activado():
+		escudo.activar()
+
 
 
 func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
